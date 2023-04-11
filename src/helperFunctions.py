4e40e7c83d4defdb0707d527c0607d8fc1e3ebf6
SanjanaCode <sanjanareddy.feb2001@gmail.com>
2023-04-11 12:41:52 -0700

    changed variable in helper

--- src/helperFunctions.py
+++ src/helperFunctions.py
@@ -2,19 +2,19 @@ from random import choice
 import time
 
 
-def calculateTimeForEachN(myFn, tVals,  tweets, abbreviations_dict, numTrials=20):
+def calculateTimeForEachT(myFn, tVals,  tweets, abbreviations_dict, numTrials=20):
     tValues = []
     timeValues = []
-    for n in tVals:
+    for t in tVals:
         # run myFn several times and average to get a decent idea.
         runtime = 0
         for t in range(numTrials):
-            lst1 = [ choice(tweets) for i in range(n) ] # generate a random list of tweets of length n
+            lst1 = [ choice(tweets) for i in range(t) ] # generate a random list of tweets of length n
             start = time.time()
             myFn( lst1, abbreviations_dict )
             end = time.time()
             runtime += (end - start) * 1000 # measure in milliseconds
         runtime = runtime/numTrials
-        tValues.append(n)
+        tValues.append(t)
         timeValues.append(runtime)
     return tValues, timeValues
